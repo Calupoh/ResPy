c762e0acc1988485801eda8d6e8b5f56b1ef1c2a
calupoh <m.moreno@calupoh.mx.nf>
2022-07-20 17:06:08 -0500

    nuevos archivos base

---
+++ ResPy_ui.bat
@@ -0,0 +1,5 @@
+::@echo off
+:: Descomentar la siguiente linea para lanzamiento
+::start bin\pythonw.exe scripts\GUI.pyw
+:: Descomentar la siguiente linea en produccion
+start bin\python.exe scripts\GUI.py
---
+++ scripts/GUI.py
@@ -0,0 +1,8 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+root.title('ResPy')
+
+if __name__ == '__main__':
+    root.mainloop()
